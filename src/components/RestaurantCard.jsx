994e72c53c7893cd0abbfcda7bb5d24a335ff31d
Michael Pyon <michaelpyon@Michaels-MacBook-Pro-3.local>
2026-04-01 00:38:16 -0400

    UI: heart icon on save button per spec (was bookmark icon)

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -79,7 +79,7 @@ export default function RestaurantCard({ restaurant, onClick, onSave, isSaved })
             stroke="currentColor"
             strokeWidth={2}
           >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
           </svg>
         </button>
 
